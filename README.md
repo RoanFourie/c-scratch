12228451eba877516d3d9cb406d80b38172ef376
Roan Fourie <roanfourie@gmail.com>
2017-01-11 09:49:00 +0200

    Strings and Message Boxes

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-## My C programming scratch repo  
+# My C programming scratch repo  
 
-# Using TCC  
+## Using TCC  
+Some of the smaller programs can be run using tcc.  
+* Use your CLI or windows command prompt  
 * CD into the TCC folder   
 ```CD c:\tcc\```  
 * Run a program with inline compilation by:  
---
+++ windows/mb-var.c
@@ -0,0 +1,21 @@
+#include <windows.h>
+#include <stdio.h>
+#include <string.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine, int nCmdShow)
+{
+    char test_string[50];
+    char *ip_str = "172.17.48.173";
+    ip_str = "172.17.48.xxx";
+    int val1, val2, val3, val4;
+    val1 = 172;
+    val2 = 17;
+    val3 = 64;
+    val4 = 173;
+    sprintf(ip_str, "%d.%d.%d.%d", val1, val2, val3, val4);
+    sprintf(test_string, "Hello test %s\n",ip_str);
+    MessageBox(NULL, test_string, "Programmed by Roan Fourie", MB_OK);
+
+
+}
